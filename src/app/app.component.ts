2a99e45e8b9b9d66ac2556d6c2dc4c4fb1ac665b
Scott Rock <scottrockbiz@gmail.com>
2018-09-12 21:19:41 -0700

    Switchboard workimg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'switchboard-app';
+  title: string = 'switchboard-app';
+  switches: Boolean[] = [];
+
+  ngOnInit() {
+    for ( let idx = 0; idx < 10; idx += 1 ) {
+      this.switches[idx] = (Math.floor(Math.random() * 2)) === 1;
+    }
+
+  }
+
+  onClick(switchNumber: number): void {
+    this.switches[switchNumber] = !this.switches[switchNumber];
+  }
 }
